3cbe8e2b6d1d81dce995e979479bf0b948070c7e
Rhodes <rbaker@lutron.com>
2016-05-12 21:04:10 -0400

    Adding index to 400

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ app.get('/webhook', function (req, res) {
     if (req.query['hub.verify_token'] === 'testbot_verify_token') {
         res.send(req.query['hub.challenge']);
     } else {
-        res.send('Invalid verify token');
-    }
+    res.sendStatus(400);
+  }
 });
